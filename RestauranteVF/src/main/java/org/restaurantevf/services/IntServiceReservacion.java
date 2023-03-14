48ed8177fd5808b28011bd1e02abb9b36abf5b6c
Aksnes <DxrkMoon@outlook.com>
2023-03-14 14:16:34 -0600

    version 1.02

--- RestauranteVF/src/main/java/org/restaurantevf/services/IntServiceReservacion.java
+++ RestauranteVF/src/main/java/org/restaurantevf/services/IntServiceReservacion.java
@@ -6,7 +6,7 @@ import org.restaurantevf.entity.Reservacion;
 
 public interface IntServiceReservacion {
 	
-	public List<n> obtenerReservacion();
+	public List<Reservacion> obtenerReservacion();
 	public void guardar(Reservacion reservacion);
 	public void eliminar(Integer idReservacion);
 	public Reservacion buscarPorId(Integer idReservacion);
