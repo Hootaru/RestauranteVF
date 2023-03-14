ec07f981a456f5d5db35086c4c5b93db986bd74e
Aksnes <DxrkMoon@outlook.com>
2023-03-14 11:54:32 -0600

    version 1

---
+++ RestauranteVF/src/test/java/org/restaurantevf/RestauranteVfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.restaurantevf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteVfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
