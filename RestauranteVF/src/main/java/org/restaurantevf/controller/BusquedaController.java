11764ff4bf395d6a4d497de1075ca5c21d4edada
Aksnes <DxrkMoon@outlook.com>
2023-03-25 22:10:54 -0600

    V2.8 - HTML

--- RestauranteVF/src/main/java/org/restaurantevf/controller/BusquedaController.java
+++ RestauranteVF/src/main/java/org/restaurantevf/controller/BusquedaController.java
@@ -55,7 +55,7 @@ public class BusquedaController {
 		Example<Restaurante> example = Example.of(restaurante, matcher);
 		List<Restaurante> lista = serviceRestaurante.buscarByExample(example);
 		model.addAttribute("vacantes", lista);
-		return "home";
+		return "busqueda";
 	}
 	
 	/**
