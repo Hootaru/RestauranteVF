24adc0a9b00c1a8ccec67b34f63fcbc1f0a0a7f0
Aksnes <DxrkMoon@outlook.com>
2023-03-25 18:29:58 -0600

    V1-25

--- RestauranteVF/src/main/java/org/restaurantevf/controller/BusquedaController.java
+++ RestauranteVF/src/main/java/org/restaurantevf/controller/BusquedaController.java
@@ -13,24 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/busquedas")
@@ -42,85 +33,12 @@ public class BusquedaController {
 	// Inyectamos una instancia desde nuestro ApplicationContext
     @Autowired
 	private RestauranteService serviceRestaurante;
-    
-    @Autowired
-   	private UsuariosService serviceUsuarios;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder;
   
 	@GetMapping("/index")
 	public String mostrarBusqueda() {
 		return "busqueda";
 	}
-	
-	@GetMapping("/inicio")
-	public String mostrarIndex(Authentication authentication, HttpSession session) {		
-		
-		// Como el usuario ya ingreso, ya podemos agregar a la session el objeto usuario.
-		String username = authentication.getName();		
-		
-		for(GrantedAuthority rol: authentication.getAuthorities()) {
-			System.out.println("ROL: " + rol.getAuthority());
-		}
-		
-		if (session.getAttribute("usuario") == null){
-			Usuario usuario = serviceUsuarios.buscarPorUsername(username);	
-			//System.out.println("Usuario: " + usuario);
-			session.setAttribute("usuario", usuario);
-		}
-		
-		return "redirect:/";
-	}
-	
-	/**
-	 * Método que muestra el formulario para que se registren nuevos usuarios.
-	 * @param usuario
-	 * @return
-	 */
-	@GetMapping("/signup")
-	public String registrarse(Usuario usuario) {
-		return "formRegistro";
-	}
-	
-	/**
-	 * Método que guarda en la base de datos el usuario registrado
-	 * @param usuario
-	 * @param attributes
-	 * @return
-	 */
-	@PostMapping("/signup")
-	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
-		// Recuperamos el password en texto plano
-		String pwdPlano = usuario.getPassword();
-		// Encriptamos el pwd BCryptPasswordEncoder
-		String pwdEncriptado = passwordEncoder.encode(pwdPlano); 
-		// Hacemos un set al atributo password (ya viene encriptado)
-		usuario.setPassword(pwdEncriptado);	
-		usuario.setEstatus(1); // Activado por defecto
-		usuario.setFechaRegistro(new Date()); // Fecha de Registro, la fecha actual del servidor
-		
-		// Creamos el Perfil que le asignaremos al usuario nuevo
-		Perfil perfil = new Perfil();
-		perfil.setId(3); // Perfil USUARIO
-		usuario.agregar(perfil);
-		
-		/**
-		 * Guardamos el usuario en la base de datos. El Perfil se guarda automaticamente
-		 */
-		serviceUsuarios.guardar(usuario);
-				
-		attributes.addFlashAttribute("msg", "Has sido registrado. ¡Ahora puedes ingresar al sistema!");
-		
-		return "redirect:/login";
-	}
-	
-	/**
-	 * Método para realizar búsquedas desde el formulario de búsqueda del HomePage
-	 * @param vacante
-	 * @param model
-	 * @return
-	 */
+
 	@GetMapping("/search")
 	public String buscar(@ModelAttribute("search") Restaurante restaurante, Model model) {
 		
@@ -140,47 +58,6 @@ public class BusquedaController {
 		model.addAttribute("vacantes", lista);
 		return "home";
 	}
-	
-	/**
-	 * Metodo que muestra la vista de la pagina de Acerca
-	 * @return
-	 */
-	@GetMapping("/about")
-	public String mostrarAcerca() {			
-		return "acerca";
-	}
-	
-	/**
-	 * Método que muestra el formulario de login personalizado.
-	 * @return
-	 */
-	@GetMapping("/login")
-	public String mostrarLogin() {
-		return "formLogin";
-	}
-	
-	/**
-	 * Método personalizado para cerrar la sesión del usuario
-	 * @param request
-	 * @return
-	 */
-	@GetMapping("/logout")
-	public String logout(HttpServletRequest request) {
-		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-		logoutHandler.logout(request, null, null);
-		return "redirect:/";
-	}
-	
-	/**
-     * Utileria para encriptar texto con el algorito BCrypt
-     * @param texto
-     * @return
-     */
-    @GetMapping("/bcrypt/{texto}")
-    @ResponseBody
-   	public String encriptar(@PathVariable("texto") String texto) {    	
-   		return texto + " Encriptado en Bcrypt: " + passwordEncoder.encode(texto);
-   	}
 	
 	/**
 	 * Metodo que agrega al modelo datos genéricos para todo el controlador
