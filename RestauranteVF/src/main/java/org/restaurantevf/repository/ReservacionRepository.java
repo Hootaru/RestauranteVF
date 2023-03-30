8373700484ac72137d48542a06b72e8877cfd0ff
Aksnes <DxrkMoon@outlook.com>
2023-03-30 00:56:24 -0600

    Cambios

--- RestauranteVF/src/main/java/org/restaurantevf/repository/ReservacionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.restaurantevf.repository;
-
-import org.restaurantevf.entity.Reservacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservacionRepository extends JpaRepository<Reservacion, Integer> {
-
-}
