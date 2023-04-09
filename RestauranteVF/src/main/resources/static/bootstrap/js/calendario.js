f928a1614b76e0a3ae27240278599a3902e47cce
Aksnes <DxrkMoon@outlook.com>
2023-04-09 03:53:32 -0500

    V2.2 - HTML

--- RestauranteVF/src/main/resources/static/bootstrap/js/calendario.js
+++ RestauranteVF/src/main/resources/static/bootstrap/js/calendario.js
@@ -1,3 +1,15 @@
-const currentDate = new Date().toISOString().split("T")[0];
-document.querySelector("#fecha-hora").setAttribute("min", currentDate);
+var fecha = new Date();
+var anio = fecha.getFullYear();
+var dia = fecha.getDate();
+var _mes = fecha.getMonth(); //viene con valores de 0 al 11
+_mes = _mes + 1; //ahora lo tienes de 1 al 12
+if (_mes < 10) //ahora le agregas un 0 para el formato date
+{
+    var mes = "0" + _mes;
+} else {
+    var mes = _mes.toString;
+}
 
+var fecha_minimo = anio + '-' + mes + '-' + dia; // Nueva variable
+document.getElementById("fechaReserva").setAttribute('min', fecha_minimo);  
+fechaReserva.min = new Date().toISOString().split("T")[0];
