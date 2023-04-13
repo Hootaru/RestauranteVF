e5925ad7596fc4c4dfb27ca429c705398b304a46
Aksnes <DxrkMoon@outlook.com>
2023-04-12 23:09:19 -0500

    V2.0 FN

--- RestauranteVF/src/main/java/org/restaurantevf/controller/ReservasController.java
+++ RestauranteVF/src/main/java/org/restaurantevf/controller/ReservasController.java
@@ -3,22 +3,21 @@ package org.restaurantevf.controller;
 import java.util.List;
 
 import org.restaurantevf.entity.Reserva;
-
+import org.restaurantevf.entity.Restaurante;
+import org.restaurantevf.entity.Usuario;
 import org.restaurantevf.services.ReservasService;
 import org.restaurantevf.services.RestauranteService;
-
+import org.restaurantevf.services.UsuariosService;
 import org.restaurantevf.util.Utileria;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.restaurantevf.entity.Restaurante;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -42,7 +42,10 @@ public class ReservasController {
 	
     // Inyectamos una instancia desde nuestro ApplicationContext
     @Autowired
-	private ReservasService reservasService;    
+	private ReservasService reservasService;  
+    
+    @Autowired
+   	private UsuariosService serviceUsuario;
     
     /**
 	 * Metodo que muestra la lista de solicitudes con paginacion
@@ -97,15 +100,18 @@ public class ReservasController {
 	 * @return
 	 */
 	@PostMapping("/save")
-	public String guardar(Reserva reserva, BindingResult result, Model model, RedirectAttributes model2,
-			@RequestParam("archivoImagen") MultipartFile file) {
-		if (result.hasErrors()) {
-			for (ObjectError error : result.getAllErrors()) {
-				System.out.println("Ocurrio un error: " + error.getDefaultMessage());
-			}
-			model.addAttribute("reservas", reservasService.buscarTodas());
-			return "reservas/formReserva";
-		}
+	public String guardar(Reserva reserva, BindingResult result, Model model, HttpSession session,
+			@RequestParam("archivoImagen") MultipartFile file, RedirectAttributes attributes, RedirectAttributes model2, Authentication authentication) {	
+		
+		// Recuperamos el username que inicio sesión
+		String username = authentication.getName();
+		
+		if (result.hasErrors()){
+			
+			System.out.println("Existieron errores");
+			return "solicitudes/formSolicitud";
+		}	
+		
 		if (reserva.getId() == null) {
 			if (!file.isEmpty()) {
 				String fileName = util.uploadImage(file);
@@ -113,20 +119,31 @@ public class ReservasController {
 					reserva.setImagen(fileName);
 				}
 			}
-			model2.addFlashAttribute("msg", "Gracias por reservar en RestauranteVF!.");
+			model2.addFlashAttribute("msg", "La información del Restaurante ha sido agregada correctamente.");
 		} else {
 			if (!file.isEmpty()) {
 				String fileName = util.uploadImage(file);
 				if (fileName != null) {
 					reserva.setImagen(fileName);
-					model2.addFlashAttribute("msg", "La información de la Reserva ha sido modificada correctamente");
+					model2.addFlashAttribute("msg", "La información del Restaurante ha sido modificada correctamente");
 				}
 			} else {
 				Reserva r = reservasService.buscarPorId(reserva.getId());
 				reserva.setImagen(r.getImagen());
 			}
 		}
+		
+		
+
+		// Buscamos el objeto Usuario en BD	
+		Usuario usuario = serviceUsuario.buscarPorUsername(username);			
+		reserva.setUsuario(usuario); // Referenciamos la solicitud con el usuario
+		
+		// Guadamos el objeto solicitud en la bd
 		reservasService.guardar(reserva);
+		attributes.addFlashAttribute("msg", "Gracias por reservar en RestauranteVF!");
+			
+		System.out.println("Reserva:" + reserva);
 		return "redirect:/";		
 	}
 	
