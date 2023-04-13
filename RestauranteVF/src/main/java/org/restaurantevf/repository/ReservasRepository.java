d7ca14adb86b9ac3bf534dc20d535b29d8d6c93c
Aksnes <DxrkMoon@outlook.com>
2023-04-12 21:52:13 -0500

    V1.0 - INICIO

--- RestauranteVF/src/main/java/org/restaurantevf/repository/ReservasRepository.java
+++
@@ -1,8 +0,0 @@
-package org.restaurantevf.repository;
-
-import org.restaurantevf.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservasRepository extends JpaRepository<Reserva, Integer> {
-
-}
